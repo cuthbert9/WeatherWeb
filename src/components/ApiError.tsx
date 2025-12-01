3399372b844b7a0277b0f4c87d7d75d4f7d1c13f
cuthbert9 <Cathbertjohnson9@gmail.com>
2025-12-01 20:43:44 +0300

    ads

--- src/components/ApiError.tsx
+++ src/components/ApiError.tsx
@@ -9,10 +9,10 @@ type ApiErrorProps = {
 
 export default function ApiError({ onRetry, message }: ApiErrorProps) {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center gap-4  text-white p-6 rounded-xl">
+    <div className="w-full h-full flex flex-col items-center justify-center gap-4 mt-20 text-white p-6 rounded-xl">
       <AlertCircle className="w-12 h-12 text-gray-400" />
 
-      <h2 className="text-2xl font-semibold">Something went wrong</h2>
+      <h2 className="text-3xl font-semibold">Something went wrong</h2>
 
       <p className="text-gray-400 text-sm text-center max-w-xs">
         {message ?? "We couldn’t connect to the server (API error). Please try again in a few moments."}
